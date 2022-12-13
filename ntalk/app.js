ab79440f6de5ef4dec389e3c0a1fb25a55727d2d
JGabriel-SL <gabrieldesouzalima44@gmail.com>
2022-12-13 02:58:34 -0300

    addind css and crud

--- ntalk/app.js
+++ ntalk/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const methodOverride = require('method-override')
+const error = require('./middleware/error');
 
 const app = express();
 
@@ -29,16 +30,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // method override - permite utilizar um mesmo path entre ps métodos HTTP
 // app.use(methodOverride());
-app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride('_method'))
 
 // config. para utlizar arquivos da pasta public
 app.use(express.static(__dirname + '/public'));
+
+
+// config routes to error pages.
+
 
 // config. para carregar os arquivos as respectivas pastas
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
+
+app.use(error.notFound);
+app.use(error.serverError);
 
 app.listen(3000, () => {
   console.log('Ntalk está funcionando corretamente!');
--- ntalk/routes/contacts.js
+++ ntalk/routes/contacts.js
@@ -1,9 +1,11 @@
 module.exports = app => {
     const { contacts } = app.controllers;
-    app.get('/contacts', contacts.index);
-    app.get('/contacts/:id', contacts.show);
-    app.post('/contacts', contacts.create);
-    app.get('/contacts/:id/edit', contacts.edit);
-    app.put('/contacts/:id', contacts.update);
-    app.delete('/contacts/:id/delete', contacts.destroy);
+    const auth = require('./../middleware/authenticator');
+    
+    app.get('/contacts', auth, contacts.index);
+    app.get('/contacts/:id', auth, contacts.show);
+    app.post('/contacts', auth, contacts.create);
+    app.get('/contacts/:id/edit', auth, contacts.edit);
+    app.put('/contacts/:id', auth, contacts.update);
+    app.delete('/contacts/:id/delete', auth, contacts.destroy);
 }
